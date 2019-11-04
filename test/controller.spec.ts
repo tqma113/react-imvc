ecc11119692fddeb61d319deec95c700d0a0e6fe
tqma <tqma@ctrip.com>
2019-11-04 20:19:29 +0800

    test(controller): add todo

--- test/controller.spec.ts
+++ test/controller.spec.ts
@@ -1,3 +1,252 @@
 describe('controller', () => {
-  it.todo('all')
+  describe('location', () => {
+    it.todo('valid')
+  })
+  
+  describe('history', () => {
+    it.todo('valid')
+  })
+  
+  describe('context', () => {
+    it.todo('valid')
+  })
+
+  describe('View', () => {
+    it.todo('valid')
+  })
+  
+  describe('Model', () => {
+    it.todo('valid')
+  })
+  
+  describe('initialState', () => {
+    it.todo('valid')
+  })
+  
+  describe('actions', () => {
+    it.todo('valid')
+  })
+
+  describe('SSR', () => {
+    it.todo('true')
+
+    it.todo('false')
+  })
+
+  describe('preload', () => {
+    it.todo('valid')
+  })
+  
+  describe('KeepAlive', () => {
+    it.todo('valid')
+  })
+  
+  describe('KeepAliveOnPush', () => {
+    it.todo('valid')
+  })
+  
+  describe('Loading', () => {
+    it.todo('valid')
+  })
+  
+  describe('API', () => {
+    it.todo('valid')
+  })
+  
+  describe('restapi', () => {
+    it.todo('valid')
+  })
+  
+  describe('resetScrollOnMount', () => {
+    it.todo('valid')
+  })
+  
+  describe('fetch', () => {
+    it.todo('valid')
+  })
+  
+  describe('get', () => {
+    it.todo('valid')
+  })
+  
+  describe('post', () => {
+    it.todo('valid')
+  })
+  
+  describe('fetchPreload', () => {
+    it.todo('valid')
+  })
+  
+  describe('prefetch', () => {
+    it.todo('valid')
+  })
+  
+  describe('fetchPreload', () => {
+    it.todo('valid')
+  })
+  
+  describe('prependBasename', () => {
+    it.todo('valid')
+  })
+  
+  describe('prependPublicPath', () => {
+    it.todo('valid')
+  })
+  
+  describe('prependRestapi', () => {
+    it.todo('valid')
+  })
+  
+  describe('getCookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('redirect', () => {
+    it.todo('valid')
+  })
+  
+  describe('reload', () => {
+    it.todo('valid')
+  })
+  
+  describe('getCookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('setCookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('removeCookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('cookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('refreshView', () => {
+    it.todo('valid')
+  })
+  
+  describe('renderView', () => {
+    it.todo('valid')
+  })
+  
+  describe('getInitialState', () => {
+    it.todo('valid')
+  })
+  
+  describe('getFinalActions', () => {
+    it.todo('valid')
+  })
+  
+  describe('cookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('shouldComponentWillCreate', () => {
+    it.todo('valid')
+  })
+  
+  describe('componentDidFirstMount', () => {
+    it.todo('valid')
+  })
+  
+  describe('componentDidMount', () => {
+    it.todo('valid')
+  })
+  
+  describe('componentWillUnmount', () => {
+    it.todo('valid')
+  })
+  
+  describe('pageWillLeave', () => {
+    it.todo('valid')
+  })
+  
+  describe('pageDidBack', () => {
+    it.todo('valid')
+  })
+  
+  describe('windowWillUnload', () => {
+    it.todo('valid')
+  })
+  
+  describe('stateDidChange', () => {
+    it.todo('valid')
+  })
+  
+  describe('stateDidReuse', () => {
+    it.todo('valid')
+  })
+  
+  describe('errorDidCatch', () => {
+    it.todo('valid')
+  })
+  
+  describe('getComponentFallback', () => {
+    it.todo('valid')
+  })
+  
+  describe('getViewFallback', () => {
+    it.todo('valid')
+  })
+  
+  describe('saveToCache', () => {
+    it.todo('valid')
+  })
+  
+  describe('removeFromCache', () => {
+    it.todo('valid')
+  })
+  
+  describe('getAllCache', () => {
+    it.todo('valid')
+  })
+  
+  describe('cookie', () => {
+    it.todo('valid')
+  })
+  
+  describe('getContainer', () => {
+    it.todo('valid')
+  })
+  
+  describe('clearContainer', () => {
+    it.todo('valid')
+  })
+  
+  describe('handleInputChange', () => {
+    it.todo('valid')
+  })
+  
+  describe('fetchPreload', () => {
+    it.todo('valid')
+  })
+  
+  describe('init', () => {
+    it.todo('valid')
+  })
+  
+  describe('destory', () => {
+    it.todo('valid')
+  })
+  
+  describe('initialize', () => {
+    it.todo('valid')
+  })
+  
+  describe('bindStoreWithView', () => {
+    it.todo('valid')
+  })
+  
+  describe('restore', () => {
+    it.todo('valid')
+  })
+  
+  describe('render', () => {
+    it.todo('valid')
+  })
+  
 })
