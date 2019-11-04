07952df49751d83582ff6ae0968bc19ee30ae649
tqma <tqma@ctrip.com>
2019-11-04 15:57:48 +0800

    test(imvc): refactor

--- test/imvc.test.ts
+++ test/imvc.test.ts
@@ -18,7 +18,7 @@ interface App extends express.Express {
 process.env.NODE_ENV = 'development'
 let PORT = 33334
 const ROOT = path.join(__dirname, 'project')
-const config: Partial<Config> = {
+const defaultConfig: Partial<Config> = {
 	root: ROOT, // 项目根目录
 	port: PORT, // server 端口号
 	logger: null, // 不出 log
@@ -35,83 +35,11 @@ const config: Partial<Config> = {
 
 describe('React-IMVC', () => {
 	describe('Enable SSR', () => {
-		mainTest({
-			...config,
+		let config = {
+			...defaultConfig,
 			port: PORT++,
 			SSR: true
-		})
-	})
-	describe('Disable SSR', () => {
-		mainTest({
-			...config,
-			port: PORT++,
-			SSR: false
-		})
-	})
-})
-function mainTest(config: Partial<Config>) {
-
-	describe('static view', () => {
-		let renderCondition = config.SSR ? 'render' : 'NOT render'
-		
-		let server: Server
-		let browser: puppeteer.Browser
-
-		beforeAll(async (done) => {
-			await start({ config }).then((result) => {
-				server = result.server
-				return puppeteer.launch()
-			}).then((brws) => {
-				browser = brws
-			})
-			done()
-		})
-	
-		afterAll(async (done) => {
-			server.close()
-			await browser.close()
-			done()
-		})
-
-		it(`should ${renderCondition} view in server side`, async (done) => {
-			let page = await browser.newPage()
-			let url = `http://localhost:${config.port}/static_view`
-			await page.goto(url)
-			await page.waitFor('#static_view')
-			let serverContent = await fetchContent(url)
-			let clientContent = await page.evaluate(
-				() => document.documentElement.outerHTML
-			)
-			expect(serverContent.includes('static view content')).toBe(
-				config.SSR ? true : false
-			)
-			expect(clientContent.includes('static view content')).toBe(true)
-
-			await page.close()
-			done()
-		}) 
-
-		it('should not render view in server side when controller.SSR is false', async (done) => {
-			let page = await browser.newPage()
-			let url = `http://localhost:${config.port}/static_view_csr`
-			await page.goto(url)
-			await page.waitFor('#static_view_csr')
-			let serverContent = await fetchContent(url)
-			let clientContent = await page.evaluate(
-				() => document.documentElement.outerHTML
-			)
-			expect(
-				serverContent.includes('static view content by client side rendering')
-			).toBe(false)
-			expect(
-				clientContent.includes('static view content by client side rendering')
-			).toBe(true)
-			await page.close()
-			done()
-		})
-	})
-
-	describe('server side', () => {
+		}
 		
 		let app: App
 		let server: Server
@@ -133,51 +61,165 @@ function mainTest(config: Partial<Config>) {
 			await browser.close()
 			done()
 		})
-		it('should pass server and app instance to every route handler', () => {
-			expect(app.isTouched).toBe(true)
-			expect(server.isTouched).toBe(true)
+
+		describe('static view', () => {
+			it(`should render view in server side`, async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view`
+				await page.goto(url)
+				await page.waitFor('#static_view')
+				let serverContent = await fetchContent(url)
+				let clientContent = await page.evaluate(
+					() => document.documentElement.outerHTML
+				)
+				expect(serverContent.includes('static view content')).toBe(
+					config.SSR ? true : false
+				)
+				expect(clientContent.includes('static view content')).toBe(true)
+	
+				await page.close()
+				done()
+			}) 
+	
+			it('should not render view in server side when controller.SSR is false', async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view_csr`
+				await page.goto(url)
+				await page.waitFor('#static_view_csr')
+				let serverContent = await fetchContent(url)
+				let clientContent = await page.evaluate(
+					() => document.documentElement.outerHTML
+				)
+				expect(
+					serverContent.includes('static view content by client side rendering')
+				).toBe(false)
+				expect(
+					clientContent.includes('static view content by client side rendering')
+				).toBe(true)
+				await page.close()
+				done()
+			})
 		})
-
-		it('should support custom server router', async (done) => {
-			let url = `http://localhost:${config.port}/my_router`
-			let response = await fetch(url)
-			let json = await response.json()
-			expect(typeof json).toBe('object')
-			expect(json.ok).toBe(true)
-			done()
+	
+		describe('server side', () => {
+			it('should pass server and app instance to every route handler', () => {
+				expect(app.isTouched).toBe(true)
+				expect(server.isTouched).toBe(true)
+			})
+	
+			it('should support custom server router', async (done) => {
+				let url = `http://localhost:${config.port}/my_router`
+				let response = await fetch(url)
+				let json = await response.json()
+				expect(typeof json).toBe('object')
+				expect(json.ok).toBe(true)
+				done()
+			})
+	
+			it('should support render custom layout', async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view`
+				await page.goto(url)
+				await page.waitFor('#static_view')
+				let serverContent = await fetchContent(url)
+				let __CUSTOM_LAYOUT__ = await page.evaluate(
+					() => window.__CUSTOM_LAYOUT__
+				)
+	
+				expect(serverContent.includes('window.__CUSTOM_LAYOUT__')).toBe(true)
+				expect(__CUSTOM_LAYOUT__).toBe(true)
+				await page.close()
+				done()
+			})
+	
+			let responseStatus = config.SSR ? 404 : 200
+			it(`should respond ${responseStatus} status code when url is not match`, async (done) => {
+				let url = `http://localhost:${config.port}/a_path_which_is_not match`
+				let response = await fetch(url)
+				expect(response.status).toBe(responseStatus)
+				done()
+			})
 		})
-
-		it('should support render custom layout', async (done) => {
-			let page = await browser.newPage()
-			let url = `http://localhost:${config.port}/static_view`
-			await page.goto(url)
-			await page.waitFor('#static_view')
-			let serverContent = await fetchContent(url)
-			let __CUSTOM_LAYOUT__ = await page.evaluate(
-				() => window.__CUSTOM_LAYOUT__
-			)
-
-			expect(serverContent.includes('window.__CUSTOM_LAYOUT__')).toBe(true)
-			expect(__CUSTOM_LAYOUT__).toBe(true)
-			await page.close()
-			done()
-		})
-
-		let responseStatus = config.SSR ? 404 : 200
-		it(`should respond ${responseStatus} status code when url is not match`, async (done) => {
-			let url = `http://localhost:${config.port}/a_path_which_is_not match`
-			let response = await fetch(url)
-			expect(response.status).toBe(responseStatus)
-			done()
+	
+		describe('controller', () => {
+			it('should have location and context properties in controller instance both server side and client side', async (done) => {
+				let url = `http://localhost:${config.port}/basic_state?a=1&b=2`
+				let page: any
+				let clientController: any
+				try {
+					page = await browser.newPage()
+	
+					await page.goto(url)
+					await page.waitFor('#basic_state')
+		
+					clientController = await page.evaluate(() => window.controller)
+				} catch (e) {
+					throw e
+				}
+				
+	
+				if (config.SSR) {
+					let serverController = global.controller
+	
+					let locationKeys = [
+						'params',
+						'query',
+						'pathname',
+						'pattern',
+						'search',
+						'raw'
+					]
+					locationKeys.forEach(key => {
+						expect(JSON.stringify(serverController.location[key])).toEqual(
+							JSON.stringify(clientController.location[key])
+						)
+					})
+	
+					let contextKeys = ['basename', 'publicPath', 'restapi']
+					contextKeys.forEach(key => {
+						expect(JSON.stringify(serverController.context[key])).toEqual(
+							JSON.stringify(clientController.context[key])
+						)
+					})
+	
+					expect(typeof serverController.context.req).toBe('object')
+					expect(typeof serverController.context.res).toBe('object')
+					expect(serverController.context.isClient).toBe(false)
+					expect(serverController.context.isServer).toBe(true)
+	
+					expect(clientController.context.isClient).toBe(true)
+					expect(clientController.context.isServer).toBe(false)
+				}
+	
+				let { location } = clientController
+	
+				expect(location.pattern).toEqual('/basic_state')
+				expect(location.pathname).toEqual('/basic_state')
+				expect(location.raw).toEqual('/basic_state?a=1&b=2')
+				expect(location.search).toEqual('?a=1&b=2')
+				expect(location.query).toEqual({ a: '1', b: '2' })
+				expect(location.params).toEqual({})
+	
+				await page.close()
+				done()
+			})
 		})
 	})
 
-	describe('controller', () => {
+	describe('Disable SSR', () => {
+		let config = {
+			...defaultConfig,
+			port: PORT++,
+			SSR: false
+		}
+			
+		let app: App
 		let server: Server
 		let browser: puppeteer.Browser
 
 		beforeAll(async (done) => {
 			await start({ config }).then((result) => {
+				app = result.app
 				server = result.server
 				return puppeteer.launch()
 			}).then((brws) => {
@@ -185,76 +227,157 @@ function mainTest(config: Partial<Config>) {
 			})
 			done()
 		})
-	
+
 		afterAll(async (done) => {
 			server.close()
 			await browser.close()
 			done()
 		})
 
-		it('should have location and context properties in controller instance both server side and client side', async (done) => {
-			let url = `http://localhost:${config.port}/basic_state?a=1&b=2`
-			let page: any
-			let clientController: any
-			try {
-				page = await browser.newPage()
-
+		describe('static view', () => {
+			it(`should not render view in server side`, async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view`
 				await page.goto(url)
-				await page.waitFor('#basic_state')
+				await page.waitFor('#static_view')
+				let serverContent = await fetchContent(url)
+				let clientContent = await page.evaluate(
+					() => document.documentElement.outerHTML
+				)
+				expect(serverContent.includes('static view content')).toBe(
+					config.SSR ? true : false
+				)
+				expect(clientContent.includes('static view content')).toBe(true)
 	
-				clientController = await page.evaluate(() => window.controller)
-			} catch (e) {
-				throw e
-			}
-			
-
-			if (config.SSR) {
-				let serverController = global.controller
-
-				let locationKeys = [
-					'params',
-					'query',
-					'pathname',
-					'pattern',
-					'search',
-					'raw'
-				]
-				locationKeys.forEach(key => {
-					expect(JSON.stringify(serverController.location[key])).toEqual(
-						JSON.stringify(clientController.location[key])
-					)
-				})
-
-				let contextKeys = ['basename', 'publicPath', 'restapi']
-				contextKeys.forEach(key => {
-					expect(JSON.stringify(serverController.context[key])).toEqual(
-						JSON.stringify(clientController.context[key])
-					)
-				})
-
-				expect(typeof serverController.context.req).toBe('object')
-				expect(typeof serverController.context.res).toBe('object')
-				expect(serverController.context.isClient).toBe(false)
-				expect(serverController.context.isServer).toBe(true)
-
-				expect(clientController.context.isClient).toBe(true)
-				expect(clientController.context.isServer).toBe(false)
-			}
-
-			let { location } = clientController
-
-			expect(location.pattern).toEqual('/basic_state')
-			expect(location.pathname).toEqual('/basic_state')
-			expect(location.raw).toEqual('/basic_state?a=1&b=2')
-			expect(location.search).toEqual('?a=1&b=2')
-			expect(location.query).toEqual({ a: '1', b: '2' })
-			expect(location.params).toEqual({})
-
-			await page.close()
-			done()
+				await page.close()
+				done()
+			}) 
+	
+			it('should not render view in server side when controller.SSR is false', async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view_csr`
+				await page.goto(url)
+				await page.waitFor('#static_view_csr')
+				let serverContent = await fetchContent(url)
+				let clientContent = await page.evaluate(
+					() => document.documentElement.outerHTML
+				)
+				expect(
+					serverContent.includes('static view content by client side rendering')
+				).toBe(false)
+				expect(
+					clientContent.includes('static view content by client side rendering')
+				).toBe(true)
+				await page.close()
+				done()
+			})
+		})
+	
+		describe('server side', () => {
+			it('should pass server and app instance to every route handler', () => {
+				expect(app.isTouched).toBe(true)
+				expect(server.isTouched).toBe(true)
+			})
+	
+			it('should support custom server router', async (done) => {
+				let url = `http://localhost:${config.port}/my_router`
+				let response = await fetch(url)
+				let json = await response.json()
+				expect(typeof json).toBe('object')
+				expect(json.ok).toBe(true)
+				done()
+			})
+	
+			it('should support render custom layout', async (done) => {
+				let page = await browser.newPage()
+				let url = `http://localhost:${config.port}/static_view`
+				await page.goto(url)
+				await page.waitFor('#static_view')
+				let serverContent = await fetchContent(url)
+				let __CUSTOM_LAYOUT__ = await page.evaluate(
+					() => window.__CUSTOM_LAYOUT__
+				)
+	
+				expect(serverContent.includes('window.__CUSTOM_LAYOUT__')).toBe(true)
+				expect(__CUSTOM_LAYOUT__).toBe(true)
+				await page.close()
+				done()
+			})
+	
+			let responseStatus = config.SSR ? 404 : 200
+			it(`should respond ${responseStatus} status code when url is not match`, async (done) => {
+				let url = `http://localhost:${config.port}/a_path_which_is_not match`
+				let response = await fetch(url)
+				expect(response.status).toBe(responseStatus)
+				done()
+			})
+		})
+	
+		describe('controller', () => {
+			it('should have location and context properties in controller instance both server side and client side', async (done) => {
+				let url = `http://localhost:${config.port}/basic_state?a=1&b=2`
+				let page: any
+				let clientController: any
+				try {
+					page = await browser.newPage()
+	
+					await page.goto(url)
+					await page.waitFor('#basic_state')
+		
+					clientController = await page.evaluate(() => window.controller)
+				} catch (e) {
+					throw e
+				}
+				
+	
+				if (config.SSR) {
+					let serverController = global.controller
+	
+					let locationKeys = [
+						'params',
+						'query',
+						'pathname',
+						'pattern',
+						'search',
+						'raw'
+					]
+					locationKeys.forEach(key => {
+						expect(JSON.stringify(serverController.location[key])).toEqual(
+							JSON.stringify(clientController.location[key])
+						)
+					})
+	
+					let contextKeys = ['basename', 'publicPath', 'restapi']
+					contextKeys.forEach(key => {
+						expect(JSON.stringify(serverController.context[key])).toEqual(
+							JSON.stringify(clientController.context[key])
+						)
+					})
+	
+					expect(typeof serverController.context.req).toBe('object')
+					expect(typeof serverController.context.res).toBe('object')
+					expect(serverController.context.isClient).toBe(false)
+					expect(serverController.context.isServer).toBe(true)
+	
+					expect(clientController.context.isClient).toBe(true)
+					expect(clientController.context.isServer).toBe(false)
+				}
+	
+				let { location } = clientController
+	
+				expect(location.pattern).toEqual('/basic_state')
+				expect(location.pathname).toEqual('/basic_state')
+				expect(location.raw).toEqual('/basic_state?a=1&b=2')
+				expect(location.search).toEqual('?a=1&b=2')
+				expect(location.query).toEqual({ a: '1', b: '2' })
+				expect(location.params).toEqual({})
+	
+				await page.close()
+				done()
+			})
 		})
 	})
-}
+})
 
 async function fetchContent(url: string): Promise<string> {
 	let response = await fetch(url)
