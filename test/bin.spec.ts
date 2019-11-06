3f3ba8bf230d5d34bf943b3360d6481d133697b3
tqma <tqma@ctrip.com>
2019-11-06 20:41:17 +0800

    feat(script): support config file written by typescript

--- test/bin.spec.ts
+++ test/bin.spec.ts
@@ -1,5 +1,47 @@
+import { exec } from 'child_process'
+import puppeteer from 'puppeteer'
+import path from 'path'
+import { setTimeOutAsync } from './util'
+
+jest.setTimeout(30000)
+
 describe('bin', () => {
-  it.todo('start')
+  let browser: puppeteer.Browser
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch()
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+  it('start with config file', async (done) => {
+    const start = path.resolve(__dirname, '../src/scripts/start.ts')
+    const config = path.resolve(__dirname, './project/imvc.config.ts')
+    const cmd = `ts-node ${start} --config ${config}`
+    let process = exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+        done()
+        return
+      }
+
+      console.log(`stdout: ${stdout}`)
+      console.log(`stdout: ${stderr}`)
+    })
+    await setTimeOutAsync(5000)
+    let page = await browser.newPage()
+    let url = `http://localhost:3333/static_view`
+    await page.goto(url)
+    await page.waitFor('#static_view')
+
+    let content = await page.$eval('#static_view', (e) => e.innerHTML)
+    expect(content).toBe('static view content')
+    
+    await page.close()
+    process.kill()
+    done()
+  })
 
   it.todo('build')
   
--- test/project/imvc.config.ts
+++ test/project/imvc.config.ts
@@ -1,7 +1,24 @@
-module.exports = {
-	root: __dirname, // 项目根目录
-	port: 3333, // server 端口号
+import path from 'path'
+import { Config } from '../../src'
+
+let PORT = 3333
+const ROOT = __dirname
+
+const config: Partial<Config> = {
+	root: ROOT, // 项目根目录
+	port: PORT, // server 端口号
 	routes: 'routes', // 服务端路由目录
 	layout: 'Layout.tsx', // 自定义 Layout
-	// bundleAnalyzer: true
+	// bundleAnalyzer: true,
+	staticEntry: 'index.html',
+	publish: '../publish',
+	output: {
+		path: path.resolve(ROOT, '../publish/static')
+	},
+	gulp: {
+		img: false
+	},
+	webpackDevMiddleware: true
 }
+
+export default config
