8f4e3f7123491fbcecde5797eb29ae2f970cfe92
tqma <tqma@ctrip.com>
2019-11-07 11:38:00 +0800

    test(scripts): add test support config file written by typescript

--- test/scripts.spec.ts
+++ test/scripts.spec.ts
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer'
 import path from 'path'
 import { setTimeOutAsync } from './util'
 
-jest.setTimeout(30000)
+jest.setTimeout(50000)
 
 describe('scripts', () => {
   let browser: puppeteer.Browser
@@ -15,32 +15,34 @@ describe('scripts', () => {
   afterAll(async () => {
     await browser.close()
   })
-  it('start with config file', async (done) => {
-    const start = path.resolve(__dirname, '../src/scripts/start.ts')
-    const config = path.resolve(__dirname, './project/imvc.config.ts')
-    const cmd = `ts-node ${start} --config ${config}`
-    let process = exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        console.log(err)
-        done()
-        return
-      }
 
-      console.log(`stdout: ${stdout}`)
-      console.log(`stdout: ${stderr}`)
+  describe('start', () => {
+    it('start with config file written with typescript', async (done) => {
+      const start = path.resolve(__dirname, '../dist/scripts/start.js')
+      const config = path.resolve(__dirname, './project/imvc.config.ts')
+      const cmd = `node ${start} --config ${config}`
+      let p = exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          console.log(err)
+          throw err
+        }
+  
+        console.log(`stdout: ${stdout}`)
+        console.log(`stdout: ${stderr}`)
+      })
+      await setTimeOutAsync(15000)
+      let page = await browser.newPage()
+      let url = `http://localhost:3333/static_view`
+      await page.goto(url)
+      await page.waitFor('#static_view')
+  
+      let content = await page.$eval('#static_view', (e) => e.innerHTML)
+      expect(content).toBe('static view content')
+      
+      await page.close()
+      p.kill()
+      done()
     })
-    await setTimeOutAsync(15000)
-    let page = await browser.newPage()
-    let url = `http://localhost:3333/static_view`
-    await page.goto(url)
-    await page.waitFor('#static_view')
-
-    let content = await page.$eval('#static_view', (e) => e.innerHTML)
-    expect(content).toBe('static view content')
-    
-    await page.close()
-    process.kill()
-    done()
   })
 
   it.todo('build')
