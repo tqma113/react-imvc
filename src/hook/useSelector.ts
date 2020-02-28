83d855c30fa1fc5118ee4bb4e47e45e46fb8313d
Ma Tianqi <mtq1997@126.com>
2020-02-28 15:50:27 +0800

    fix(hook): useSelector

--- src/hook/useSelector.ts
+++ src/hook/useSelector.ts
@@ -1,7 +1,7 @@
-import { useMemo, useState, useEffect } from 'react'
-import { Store } from 'relite'
-import useCtrl from './useCtrl'
-import { isFunction } from 'util'
+import { useMemo, useState, useEffect } from "react"
+import { Store } from "relite"
+import useCtrl from "./useCtrl"
+import { isFunction } from "util"
 
 export type Selector<S extends {}, R> = (state: S) => R
 
@@ -11,37 +11,41 @@ const identify = <S>(state: any) => state as S
 
 const refEquality = <T>(a: T, b: T) => a === b
 
-function useSelectorWithStore<S extends {}, R>(selector: Selector<S, R>, isEqual: EqualityFun<R>, store: Store<any, any>) {
-    const [selectState, setSelectState] = useState(selector(store.getState()))
+function useSelectorWithStore<S extends {}, R>(
+  selector: Selector<S, R>,
+  isEqual: EqualityFun<R>,
+  store: Store<any, any>
+) {
+  const [selectState, setSelectState] = useState(selector(store.getState()))
 
-    const unsubscribe = useMemo(() => {
-        return store.subscribe(() => {
-            const state = store.getState()
-            const newSelectState = selector(state)
-            if(!isEqual(newSelectState, selectState)) {
-                setSelectState(newSelectState)
-            }
-        })
-    }, [store])
+  const unsubscribe = useMemo(() => {
+    return store.subscribe(() => {
+      const state = store.getState()
+      const newSelectState = selector(state)
+      if (!isEqual(newSelectState, selectState)) {
+        setSelectState(newSelectState)
+      }
+    })
+  }, [store])
 
-    useEffect(() => {
-        return () => unsubscribe()
-    }, [])
+  useEffect(() => {
+    return () => unsubscribe()
+  }, [])
 
-    return selectState
+  return selectState
 }
 
 function createSelectorHook() {
+  return <S extends {}, R>(
+    selector: Selector<S, R> = identify,
+    isEqual: EqualityFun<R> = refEquality
+  ) => {
     const { store } = useCtrl()
-    return <S extends {}, R>(
-      selector: Selector<S, R> = identify,
-      isEqual: EqualityFun<R> = refEquality
-    ) => {
-        if(!isFunction(selector)) {
-            throw new Error('参数必须是一个function')
-        }
-        return useSelectorWithStore<S, R>(selector, isEqual, store)
+    if (!isFunction(selector)) {
+      throw new Error("参数必须是一个function")
     }
+    return useSelectorWithStore<S, R>(selector, isEqual, store)
+  }
 }
 
 const useSelector = createSelectorHook()
