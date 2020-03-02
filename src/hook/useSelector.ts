e54de420284d9181ca9cf5a97818504a05a1380c
tqma <tqma@ctrip.com>
2020-03-02 18:56:09 +0800

    update

--- src/hook/useSelector.ts
+++ src/hook/useSelector.ts
@@ -1,7 +1,11 @@
-import { useMemo, useState, useEffect } from "react"
+import { useMemo, useRef, useEffect } from "react"
 import { Store } from "relite"
 import useCtrl from "./useCtrl"
 import { isFunction } from "util"
+
+export type Pair = [any, (state: any) => void]
+
+export type Listener<R> = (pair: Pair, state: R) => void
 
 export type Selector<S extends {}, R> = (state: S) => R
 
@@ -14,37 +18,47 @@ const refEquality = <T>(a: T, b: T) => a === b
 function useSelectorWithStore<S extends {}, R>(
   selector: Selector<S, R>,
   isEqual: EqualityFun<R>,
-  store: Store<any, any>
+  useStore: () => Store<S, any>
 ) {
-  const [selectState, setSelectState] = useState(selector(store.getState()))
+  const store = useStore()
+  const currentSelectedState = useRef(selector(store.getState()))
+  const setSelectedState = (state: R) => currentSelectedState.current = state
+
+  let selectedState: R = currentSelectedState.current
 
   const unsubscribe = useMemo(() => {
-    return store.subscribe(() => {
-      const state = store.getState()
-      const newSelectState = selector(state)
-      if (!isEqual(newSelectState, selectState)) {
-        setSelectState(newSelectState)
+    return store.subscribe((data) => {
+      const newSelectedState = selector(data.currentState)
+      if (!isEqual(newSelectedState, currentSelectedState.current)) {
+        setSelectedState(newSelectedState)
       }
     })
-  }, [store])
+  }, [store.getState()])
+
+  useEffect(() => {
+    selectedState = currentSelectedState.current
+  }, [currentSelectedState.current])
 
   useEffect(() => {
     return () => unsubscribe()
-  }, [])
+  }, [store])
 
-  return selectState
+  return selectedState
 }
 
 function createSelectorHook() {
+  const useStore = () => {
+    const { store } = useCtrl()
+    return store
+  }
   return <S extends {}, R>(
     selector: Selector<S, R> = identify,
     isEqual: EqualityFun<R> = refEquality
   ) => {
-    const { store } = useCtrl()
     if (!isFunction(selector)) {
       throw new Error("参数必须是一个function")
     }
-    return useSelectorWithStore<S, R>(selector, isEqual, store)
+    return useSelectorWithStore<S, R>(selector, isEqual, useStore)
   }
 }
 
--- test/project/src/hook/Model.ts
+++ test/project/src/hook/Model.ts
@@ -1,16 +1,43 @@
 import { BaseState, Action } from '../../../../src'
 
 export type State = BaseState & {
-  foo: number
+  base: number,
+  foo: number,
+  bar: number,
+  baz: number
 }
 
 export const initialState = {
-  foo: 0
+  base: 0,
+  foo: 0,
+  bar: 0,
+  baz: 0
 }
 
-export const UPDATE_FOO: Action<State, number> = (state, foo) => {
+export const UPDATE_BASE: Action<State> = (state) => {
   return {
     ...state,
-    foo
+    base: state.base + 1
+  }
+}
+
+export const UPDATE_FOO: Action<State> = (state) => {
+  return {
+    ...state,
+    foo: state.foo + 1
+  }
+}
+
+export const UPDATE_BAR: Action<State> = (state) => {
+  return {
+    ...state,
+    bar: state.bar + 1
+  }
+}
+
+export const UPDATE_BAZ: Action<State> = (state) => {
+  return {
+    ...state,
+    baz: state.baz + 1
   }
 }
