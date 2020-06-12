d4ca32acca19f36ae6537e7f7f1ee7398f162c47
tqma <tqma@trip.com>
2020-06-12 16:47:25 +0800

    update

--- test/scripts.spec.ts
+++ test/scripts.spec.ts
@@ -19,16 +19,17 @@ describe('scripts', () => {
 
   describe('start', () => {
     it('start with config file written with typescript', async (done) => {
-      const start = path.resolve(__dirname, '../src/bin/scripts.ts')
+      const start = path.resolve(__dirname, '../bin/scripts.js')
       const config = path.resolve(__dirname, './project/imvc.config.ts')
-      const cmd = `ts-node ${start} --config ${config}`
+      const cmd = `${start} start --config ${config}`
       let p = exec(cmd, (err, stdout, stderr) => {
         if (err) {
           console.log(err)
           return
         }
+        console.log(stdout, stderr)
       })
-      await setTimeOutAsync(15000)
+      await setTimeOutAsync(5000)
       let page = await browser.newPage()
       let url = `http://localhost:33336/static_view`
       await page.goto(url)
