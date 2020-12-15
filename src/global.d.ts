678709be87f3f8d802c582ee88ac45219cd01330
tqma <tqma@trip.com>
2020-12-15 11:10:13 +0800

    update @types/webpack-manifest-plugin

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,27 +8,3 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test'
   }
 }
-
-declare module 'webpack-manifest-plugin' {
-  import { WebpackPluginInstance, Compiler } from 'webpack'
-  import { SyncWaterfallHook } from 'tapable'
-  export type Options = {
-    fileName: string
-    map: (file: FileDescriptor) => FileDescriptor
-    publicPath?: string
-  }
-  export type FileDescriptor = {
-    name: string
-  }
-  export class WebpackManifestPlugin extends WebpackPluginInstance {
-    constructor(options: Options)
-    apply(compiler: Compiler): void
-  }
-
-  export function getCompilerHooks(
-    compiler: Compiler
-  ): {
-    afterEmit: SyncWaterfallHook
-    beforeEmit: SyncWaterfallHook
-  }
-}
